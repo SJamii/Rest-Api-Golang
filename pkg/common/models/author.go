248cdfb9926a43a829c81f5446f179eb15e0e92a
SJamii <shafayatiuc@gmail.com>
2022-12-29 12:30:43 +0600

    uuid added

--- pkg/common/models/author.go
+++ pkg/common/models/author.go
@@ -1,12 +1,14 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Author struct {
 	gorm.Model
-	Name  string `json:"name" binding:"required" gorm:"not null"`
-	Age   string `json:"age" binding:"required" gorm:"not null"`
-	Books []Book `gorm:"foreignKey:AuthorRefer"`
+	ID    uuid.UUID `gorm:"primaryKey;type:uuid"`
+	Name  string    `json:"name" binding:"required" gorm:"not null"`
+	Age   string    `json:"age" binding:"required" gorm:"not null"`
+	Books []Book    `gorm:"foreignKey:AuthorRefer"`
 }
--- pkg/common/models/book.go
+++ pkg/common/models/book.go
@@ -1,11 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Book struct {
 	gorm.Model
-	Title string `json:"title"`
-	// Author      string `json:"author"`
-	Description string `json:"description"`
-	AuthorRefer uint
+	ID          uuid.UUID `gorm:"primaryKey;type:uuid"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	AuthorRefer uuid.UUID
 }
